d6d2277d693da0ed8bc94573d16eae00012ee9bc
Sahid Rahman <sahidrahman404@gmail.com>
2025-11-12 21:07:19 +0700

    update config

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
 export const config = {
   sessionSecret: process.env.SESSION_SECRET || 'rahasia',
   sessionCookieName: process.env.SESSION_COOKIE_NAME || 'sid',
+  frontendUrl: process.env.FRONTEND_URL || 'http://localhost:3001',
+  nodeEnv: process.env.NODE_ENV || 'development',
 };
